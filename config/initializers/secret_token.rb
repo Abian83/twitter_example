b4342ae6f9c6ea1a17b0b3d6b102b7db81120f27
Abian <abian83@gmail.com>
2015-01-17 21:14:03 +0000

    Initial commit, created RoRº app template.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterExample::Application.config.secret_token = 'cc042063c2130edf6be230638afeb1fe5d2871a7daa1423d77018fc9c623ee1efda1142df8fd0a88cccd044c534dd99f9361f52db6196067c8b87d7b067edac7'
